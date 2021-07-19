00dfaafe95ce132fcf4ba80d72a104322406a048
Cloe Lhenry <cloelh@outlook.fr>
2021-07-19 11:40:53 +0200

    datanase

--- src/App/Aware/AwareManager.php
+++ src/App/Aware/AwareManager.php
@@ -33,7 +33,8 @@ class AwareManager
 
         if ($object instanceof DatabaseConnectionAware) {
             if (!$this->databaseConnection) {
-                $this->databaseConnection = new PDO('mysql:dbname=MemeSharrr;host=localhost;charset=utf8', 'root', 'root');
+                // $this->databaseConnection = new PDO('mysql:dbname=MemeSharrr;host=localhost;charset=utf8', 'root', 'root');
+                $this->databaseConnection = new PDO('postgres://kormjsjhynjjfj:67135a414a33b079b24e41b88bf8a01760c6be3c7b4c81ae04671dd5b2ec164d@ec2-54-154-101-45.eu-west-1.compute.amazonaws.com:5432/dck3ohp8rfuha0');
             }
 
             $object->provideDatabaseConnection($this->databaseConnection);
